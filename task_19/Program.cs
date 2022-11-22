c0efe91d90c6b00906faea4994d709df80e5b86f
evg.tsl <evg.tsl@ya.ru>
2022-11-22 15:17:23 +0600

    add homework

---
+++ task_19/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да*/
+
+void Palindrom()
+{
+    Console.WriteLine("Введите Ваше число");
+    var numb = Convert.ToString(Console.ReadLine());
+    int arr = numb.Length;
+    
+    if (arr == 5)
+    {
+        if (numb[0] == numb [4] && numb [1] == numb[3])
+        {
+            Console.WriteLine("Введенное число является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Введенное число не является палиндромом.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введенное число не пятизначное.");
+    }
+}
+Palindrom();
